374a7d4d6214d9b509882b381ec98cd0978e3181
Craig Swank <craigswank@gmail.com>
2014-01-06 08:57:08 -0700

    fixed import in app

--- app/brewery.go
+++ app/brewery.go
@@ -3,7 +3,7 @@ package main
 
 import (
 	"bitbucket.com/cswank/gogadgets"
-	"bitbucket.com/cswank/brewery/gogadgets"
+	"bitbucket.com/cswank/brewery/gadgets"
 	"encoding/json"
 	"io/ioutil"
 	"flag"
@@ -22,18 +22,18 @@ func main() {
 	err = json.Unmarshal(b, cfg)
 	a := gogadgets.NewApp(cfg)
 
-	config = &MashConfig{
+	config = &gadgets.MashConfig{
 		TankRadius: 7.5 * 2.54,
 		ValveRadius: 0.1875 * 2.54,
 		Coefficient: 0.43244,
 	}
-	mash, _ := NewMash(config)
+	mash, _ := gadgets.NewMash(config)
 	a.AddGadget(mash)
 	poller, err := NewGPIO(&Pin{Port:"8", Pin:"9", Direction:"in", Edge:"rising"})
 	if err != nil {
 		panic(err)
 	}
-	hlt := &HLT{
+	hlt := &gadgets.HLT{
 		GPIO: poller,
 		volume: 26.5,
 		units: "liters",
